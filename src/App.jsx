c7fa8cfce6e6e8d8b3ffac94a87fa3a2036997bc
Lloyd <Lloydabonds@gmail.com>
2024-05-22 13:36:55 -0400

    tasks reamining

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import Todo from "./components/Todo.jsx"
 import Form from "./components/Form.jsx"
 import FilterButton from "./components/FilterButton.jsx"
 import {useState} from "react"
+import {nanoid} from "nanoid"
 
 function App  (props) {
 
   const [tasks, setTasks] = useState(props.tasks)
 
   function addTask(name){
-    const newTask = {id: "id", name, completed:false}; 
+    const newTask = {id: `todo-${nanoid()}`, name, completed:false}; 
     setTasks([...tasks, newTask])
   }
 
@@ -16,13 +17,16 @@ function App  (props) {
   const taskList = tasks?.map((task) => (
     <Todo id={task.id} name ={task.name} completed={task.completed} key={task.id}/>
   ));
+  
+  const tasksNoun = taskList.length == 1? "task" : "tasks";  
+  const headingText = `${taskList.length} ${tasksNoun} remaining`; 
 
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
       <Form addTask={addTask} />
       <FilterButton /> 
-      <h2 id="list-heading">3 tasks remaining</h2>
+      <h2 id="list-heading">{headingText}</h2>
       <ul role="list" className="todo-list stack-large stack-exception" aria-labelledby="list-heading">
         {taskList}
       </ul>
